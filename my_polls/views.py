7ad9bc0455670b96185cfdf493445192b03adb18
Guillermo Amarante <gac317@gmail.com>
2023-08-12 11:29:33 +0800

    Render views with shortcuts, next apply to all

--- my_polls/views.py
+++ my_polls/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-# from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # template = loader.get_template("my_polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
         }
@@ -15,13 +12,17 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # try:
+        # question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+        # raise Http404("Question does not exist, yo.")
+    return render(request, "my_polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    response = "You're seeing the results of question %s."
-    return HttpResponse(response % question_id)
+    return render(request, "my_polls/results.html")
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return render(request, "my_polls/vote.html")
