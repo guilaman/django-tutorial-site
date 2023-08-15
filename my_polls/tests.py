44611954d5766e4bf414650ec11e88ca35a874a3
Guillermo Amarante <gac317@gmail.com>
2023-08-15 08:15:06 +0800

    Write QuestionDetailView tests, end part 5

--- my_polls/tests.py
+++ my_polls/tests.py
@@ -68,6 +68,28 @@ class QuestionIndexViewTests(TestCase):
                                  [question1, question2],
                                  )
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with pub_date in the future returns a
+        404 not found.
+        """
+        future_question = create_question(question_text="Future question.",
+                                          days=5)
+        url = reverse("my_polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        Detail view of question with past pub_date displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.",
+                                        days=-5)
+        url = reverse("my_polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
